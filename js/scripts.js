80e07c40081dba37574102e5c00ca8001ceda110
aboleez <127515470+aboleez@users.noreply.github.com>
2023-05-24 22:20:12 +0530

    Code formatting
    Rearrangement and code formatting for js file and added scroll-behavior: smooth
    to the css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-
-
-var age;
-age = 15
-
-if (age < 18) {
-    console.log ('You cannot enter the cafe');
-}
-
-else if ( age > 18 && age < 23) {
-console.log ('You may enter the cafe but cannot drink')
-}
-
-else (age >= 25) {
-    console.log ('You can enter the cafe and enjoy your drinks')
-}
+function menuToggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+      x.className += " responsive";
+    } else {
+      x.className = "navtoggle";
+    }
+  }
